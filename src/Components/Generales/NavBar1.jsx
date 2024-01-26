2314db5c213fc198bcf38d3a988e1080e48cf432
Meli Barrios <melissa.blecona@gmail.com>
2024-01-26 14:33:39 -0600

    actualizacion de navbar

--- src/Components/Generales/NavBar1.jsx
+++ src/Components/Generales/NavBar1.jsx
@@ -3,97 +3,60 @@ import imgLogo from '../../assets/logo/logo.png'
 
 const NavBar1 = () => {
   return (
-     <div className="container-fluid mw-100  fixed-top ">
-    <nav className="navbar navbar-expand-lg bg-info  ">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
+  <nav className="navbar navbar-expand-lg fixed-top">
+  <div className="container-fluid">
+  <Link className="navbar-brand me-auto" to="/">
           <img src={imgLogo} alt="" 
            width={50}
            height={50}/>
-        </Link>
-        <button
-          className="navbar-toggler text-info-emphasis"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="">
-                Inicio
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="sobre-nosotros">
-                Sobre nosotros
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/servicios">
-                Servicios
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contacto">
-                Contacto
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="d-flex gap-2 d-md-flex justify-content-md-end">
-          <li className="nav-item dropdown d-flex ">
-            <Link
-              className="nav-link dropdown-toggle"
-              role="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <img src="" alt="" width="25" className="" />
-            </Link>
-
-            <ul className="dropdown-menu dropdown-menu-end">
-              <div className="d-flex">
-                <li>
-                  <Link to="/" className="dropdown-item  ">
-                    <button
-                      type="button"
-                      className="btn btn-outline-dark me-1"
-                    >
-                      Inicio de sesion
-                    </button>
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/" className="dropdown-item ">
-                    <button type="button" className="btn btn-outline-danger">
-                      Regístrate
-                    </button>
-                  </Link>
-                </li>
-              </div>
-              <li>
-                <Link to="/" className="dropdown-item">
-                  Cuentas
-                </Link>
-              </li>
-            </ul>
-          </li>
-
-          <Link>
-            <i className="bi bi-person-circle"></i>
-          </Link>
-        </div>
+  </Link>  
+ 
+    <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div className="offcanvas-header">
+        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Name</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" />
       </div>
-    </nav>
-  </div> 
+      <div className="offcanvas-body">
+        <ul className="navbar-nav justify-content-center flex-grow-1 pe-3">
+          <li className="nav-item">
+            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Sobre nosotros</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Servicios</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Contacto</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Preguntas frecuentes</a>
+          </li>
+          {/* <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Perfil
+            </a>
+            <ul className="dropdown-menu">
+              <li><a className="dropdown-item" href="#">Regístrate</a></li>
+              <li><a className="dropdown-item" href="#">Iniciar sesión</a></li>
+              <li>
+                <hr className="dropdown-divider" />
+              </li>
+              <li><a className="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li> */}
+        </ul>
+      </div>
+      </div>
+      <Link to='' className='login-button'>Login</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon" />
+    </button>
+      </div>
+      </nav>
 
+    
 
   )
 }
