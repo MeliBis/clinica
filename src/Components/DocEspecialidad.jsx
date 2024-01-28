0063e3a4760ac7a05312f19877e4f6e275392783
Meli Barrios <melissa.blecona@gmail.com>
2024-01-27 19:11:53 -0600

    actualizacion de nav con estilos css

--- src/Components/DocEspecialidad.jsx
+++ src/Components/DocEspecialidad.jsx
@@ -3,9 +3,9 @@ const DocEspecialidad = () => {
  <>
  <div className="row">
   <div className="col">
-    <label htmlFor="select1">Especialidades</label>
+    <label htmlFor="select1" className="h5">Especialidades</label>
     <select className="form-select form-select-sm" id="select1">
-      <option >Especialidades</option>
+     {/*  <option >Especialidades</option> */}
       <option value={1}>One</option>
       <option value={2}>Two</option>
       <option value={3}>Three</option>
--- src/Components/Generales/NavBar1.jsx
+++ src/Components/Generales/NavBar1.jsx
@@ -1,39 +1,56 @@
-import { Link } from 'react-router-dom'
-import imgLogo from '../../assets/logo/logo.png'
+import { Link } from "react-router-dom";
+import imgLogo from "../../assets/logo/logo.png";
 
 const NavBar1 = () => {
   return (
-  <nav className="navbar navbar-expand-lg fixed-top">
-  <div className="container-fluid">
-  <Link className="navbar-brand me-auto" to="/">
-          <img src={imgLogo} alt="" 
-           width={50}
-           height={50}/>
-  </Link>  
- 
-    <div className="offcanvas offcanvas-end" tabIndex={-1} id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasNavbarLabel">Name</h5>
-        <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close" />
-      </div>
-      <div className="offcanvas-body">
-        <ul className="navbar-nav justify-content-center flex-grow-1 pe-3">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">Inicio</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Sobre nosotros</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Servicios</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Contacto</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Preguntas frecuentes</a>
-          </li>
-          {/* <li className="nav-item dropdown">
+    /* navbar */
+    <nav className="navbar navbar-expand-lg fixed-top">
+      <div className="container">
+        <Link className="navbar-brand me-auto" to="/">
+          <img src={imgLogo} alt="" width={50} height={50} />
+        </Link>
+
+        <div
+          className="offcanvas offcanvas-end"
+          tabIndex={-1}
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+        >
+          <div className="offcanvas-header">
+            <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
+              Name
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            />
+          </div>
+          <div className="offcanvas-body">
+            <ul className="navbar-nav justify-content-center flex-grow-1 pe-3">
+              <li className="nav-item">
+                <Link className="nav-link active" aria-current="page" to="/">
+                  Inicio
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sobre-nosotros">
+                  Sobre nosotros
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/servicios">
+                  Servicios
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contacto">
+                  Contacto
+                </Link>
+              </li>
+
+              {/* <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Perfil
             </a>
@@ -46,19 +63,29 @@ const NavBar1 = () => {
               <li><a className="dropdown-item" href="#">Something else here</a></li>
             </ul>
           </li> */}
-        </ul>
+            </ul>
+          </div>
+        </div>
+        <Link to="" className="login-button">
+          Login
+        </Link>
+        <button
+          className="navbar-toggler pe-0"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasNavbar"
+          aria-controls="offcanvasNavbar"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
       </div>
-      </div>
-      <Link to='' className='login-button'>Login</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-      </div>
-      </nav>
+    </nav>
+  /* end navbar */
 
-    
 
-  )
-}
 
-export default NavBar1
+  );
+};
+
+export default NavBar1;
