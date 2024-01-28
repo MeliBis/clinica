e11c0e1b61eccb2b0080b9db2b60f1fca4b26590
Meli Barrios <melissa.blecona@gmail.com>
2024-01-27 21:18:37 -0600

    section de testimonios responsive lista

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../src/styles/navStyle.css'
-
+import '../src/styles/testimonialsStyle.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
