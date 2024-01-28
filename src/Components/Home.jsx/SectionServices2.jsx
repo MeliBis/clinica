e23887faa0a4d729f9257fe110f340127fdb129b
Meli Barrios <melissa.blecona@gmail.com>
2024-01-27 19:36:21 -0600

    seccion de servicios realizada

--- src/Components/Home.jsx/SectionServices2.jsx
+++ src/Components/Home.jsx/SectionServices2.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import img1 from '../../assets/Servicios/1.jpg'
+import img2 from '../../assets/Servicios/2.jpg'
+import img3 from '../../assets/Servicios/3.jpg'
 
 const SectionServices2 = () => {
   return (
-   <h1>servicios</h1>
+   <div className="container mb-5">
+      <div className="row g-3">
+        <h1 className='text-center'>Servicios</h1>
+        <div className="col-12 col-md-6 col-lg-4">
+          <div className="card">
+            <img src={img1} alt="foto 1" />
+            <div className="card-body">
+              <p className="card-title">Opción 1</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 col-lg-4">
+        <div className="card">
+            <img src={img2} alt="foto 1" />
+            <div className="card-body">
+              <p className="card-title">Opción 1</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-12 col-md-6 col-lg-4">
+        <div className="card">
+            <img src={img3} alt="foto 1" />
+            <div className="card-body">
+              <p className="card-title">Opción 1</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+   </div>
   )
 }
 
