e8adc0869965cc31a051c869aae9175ea1614154
Meli Barrios <melissa.blecona@gmail.com>
2024-01-29 21:04:22 -0600

    eliminacion de datos no necesarios

--- src/Components/DocEspecialidad.jsx
+++ src/Components/DocEspecialidad.jsx
@@ -4,15 +4,7 @@ const DocEspecialidad = ({ index, id, name, specialty }) => {
   return (
     <>
       <div className="row">
-        <div className="col">
-          <h2>Especialidades</h2>
-          <select className="form-select form-select-sm" aria-label="Default select example">
-            <option selected>Open this select menu</option>
-            <option value={1}><a href="/contacto">primer</a></option>
-            <option value={2}>Two</option>
-            <option value={3}>Three</option>
-          </select>
-        </div>
+   
       </div>
     </>
   );
--- src/Components/Especialidades/Espcialidad.jsx
+++ src/Components/Especialidades/Espcialidad.jsx
@@ -6,14 +6,9 @@ const Espcialidad = () => {
     <div className='App'>
       <h2 className="text-center">Doctores</h2>
       <div className="container">
-        <div className="row">          
-            <Filter/>    
-          <div className="col-8">
             <div className="row">
               <CardSpeciality/>
             </div>
-          </div>
-        </div>
       </div>
 
 
